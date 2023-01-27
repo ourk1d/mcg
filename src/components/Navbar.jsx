ca9cfab5ddd0d4499d7d7e49beb391f5f9df0962
OurKid <perdomovalentin2009@gmail.com>
2023-01-27 02:11:07 -0300

    prueba 69

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -116,7 +116,7 @@ const Navbar = () => {
     <Container>
         <Wrapper>
             <Left>
-            <Link href={`/`}>
+            <Link href={`/mcg/`}>
                 <Img src="https://i.ibb.co/PGTPQGK/Logo.png" />
             </Link>
             </Left>
@@ -144,10 +144,10 @@ const Navbar = () => {
                 <Left></Left>
                 <Right>
                 <Links2>
-                    <Link href={`/automoviles`}>Automoviles</Link>
+                    <Link href={`/mcg/automoviles`}>Automoviles</Link>
                 </Links2>
                 <Links2>
-                    <Link href={`/asasas`}>Motocicletas</Link>
+                    <Link href={`/mcg/asasas`}>Motocicletas</Link>
                 </Links2>
                 <Links2>
                     <Link href="">Camiones</Link>
