5a5c59b9f119dc3e6e60bb1a59c83e6c87b2a38a
OurKid <perdomovalentin2009@gmail.com>
2023-01-27 00:26:37 -0300

    prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,16 @@ import {
 
 
 const App = () => {
-  return <Router>
-  <Routes>
-    <Route path='/' element={<Home/>}>
-    </Route>
-    <Route path='/automoviles' element={ <Cotizacion/>}>
-    </Route>
-  </Routes>
-</Router>;
+  return (
+  <Router>
+    <Routes>
+        <Route path='/' element={<Home/>}>
+        </Route>
+        <Route path='/automoviles' element={ <Cotizacion/>}>
+        </Route>
+    </Routes>
+  </Router>
+  )
 };
 
 export default App;
