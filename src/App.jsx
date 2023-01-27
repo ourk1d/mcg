ccabb753aa4c59db07e73d6a4be4edf5cc8cbb59
OurKid <perdomovalentin2009@gmail.com>
2023-01-27 04:15:58 -0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import { Routes, Route, HashRouter as Router } from "react-router-dom";
 const App = () => {
 
   return (
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}>
         </Route>
         <Route path='/automoviles' element={ <Cotizacion/>}>
         </Route>
       </Routes>
-
+    </Router>
        )
 };
 
